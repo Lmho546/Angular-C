40d99b1e34c054592dd130c3ced5fe0b1eb6035f
Lmho546 <Lmho546P@outlook.com>
2023-03-29 22:39:07 -0300

    Segundo Commit (4)

--- src/app/resultados-busqueda/resultados-busqueda.component.ts
+++ src/app/resultados-busqueda/resultados-busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Producto } from '../../classes/producto.class';
 
 @Component({
   selector: 'app-resultados-busqueda',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./resultados-busqueda.component.css']
 })
 export class ResultadosBusquedaComponent {
+
+  productos : Array<Producto> = [
+
+   new Producto ("Danmachi Volumen 7 (Español)", "Danmachi", "Manga", 4640, 10),
+   new Producto ("Dragon Ball Super Cap 41-52 (Sub-Español)", "Dragon Ball Super", "Anime", 9500, 10),
+   new Producto ("7 Esferas De Dragon Ball", "Dragon Ball", "Coleccionable", 10000, 0),
+   new Producto ("Peluche Pichu Pokemon", "Pokemon", "Coleccionable", 6000, 10)
+
+  ];
 
 }
---
+++ src/classes/producto.class.ts
@@ -0,0 +1,10 @@
+export class Producto {
+    constructor(
+      public nombreProducto: string,
+      public anime: string,
+      public tipo: string,
+      public precio: number,
+      public stock: number,
+    ) {}
+  }
+  
